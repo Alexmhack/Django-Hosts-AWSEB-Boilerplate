0d241ebac4f2f9f0e514119ede39a9f05f639178
alexHooper <alexmohack@gmail.com>
2019-06-28 10:53:14 +0530

    include apps url

--- boilerplate/urls.py
+++ boilerplate/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from .views import index_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index_view, name='index'),
+    path('blog/', include('blog.urls', namespace='blog')),
+    path('help/', include('help.urls', namespace='help')),
 ]
