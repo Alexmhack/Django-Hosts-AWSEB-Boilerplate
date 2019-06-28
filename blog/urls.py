e1132877c32042f59da9fdb17e8c832755b49e7f
alexHooper <alexmohack@gmail.com>
2019-06-28 11:46:10 +0530

    add more blog urls

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,8 @@ app_name = 'blog'
 
 urlpatterns = [
 	path('home/', home_view, name='home'),
+	path('about/', home_view, name='about'),
+	path('list/', home_view, name='list'),
+	path('detail/', home_view, name='detail'),
+	path('new/', home_view, name='new'),
 ]
