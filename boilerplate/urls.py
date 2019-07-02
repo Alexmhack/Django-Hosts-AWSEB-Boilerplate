81367fc5ecb0009fd5a7fda423f5e76da918f402
alexHooper <alexmohack@gmail.com>
2019-07-02 20:12:50 +0530

    use login urls

--- boilerplate/urls.py
+++ boilerplate/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 from .views import index_view
 
@@ -8,4 +9,9 @@ urlpatterns = [
     path('', index_view, name='index'),
     path('blog/', include('blog.urls', namespace='blog')),
     path('help/', include('help.urls', namespace='help')),
+]
+
+urlpatterns += [
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
